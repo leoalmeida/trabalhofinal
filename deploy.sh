3e1f5dd7165efe4ee1b648eb193e5ea6e87f0ce3
Leonardo Almeida silva ferreira <leoalmeida.rj@gmail.com>
2016-05-08 18:19:10 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -117,6 +117,31 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd - > /dev/null
 fi
 
+:: 4. Install bower packages
+IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd .\node_modules\.bin\bower install
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+
+:: 5. Run gulp transformations
+IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd .\node_modules\.bin\gulp build
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 6. Run gulp transformations
+IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd .\node_modules\.bin\gulp
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
 ##################################################################################################################################
 
 # Post deployment stub
